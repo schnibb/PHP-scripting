660d0d453f4190abca41b1855e1f17d46dbfa47e
schnibb <32277572+schnibb@users.noreply.github.com>
2017-10-13 14:37:35 -0400

    Update viewDive.php

--- viewDive.php
+++ viewDive.php
@@ -25,8 +25,8 @@
     <th>Notes</th>
   </tr>
 <?php
-$database = 'ds6225';
-$db = mysqli_connect('', 'ds6225', 'emE0J3B5', $database);
+$database = 'username';
+$db = mysqli_connect('', 'username', 'password', $database);
 
 
 $id= $_GET["id"];
